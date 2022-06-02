cf0c562b9b022d99627d109a15fe467bdcbbe331
Your Name <you@example.com>
2022-06-01 19:32:39 -0700

    add six

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-for i in range(10):
-    for j in range(10):
-        if j > i:
-            if i == 8 and j == 9:
-                print("{}{}".format(i, j))
-            else:
-                print("{}{}, ".format(i, j), end="")
+if __name__ == "__main__":
+    for num1 in range(8):
+        for num2 in range(10):
+            if num1 < num2:
+                print("{:d}{:d}, ".format(num1, num2), end="")
+    print(89)
