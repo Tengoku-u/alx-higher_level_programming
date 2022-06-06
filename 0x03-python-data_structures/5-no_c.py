cff8cee771e6ed904e59cc8a43bbc7397943afb2
Your Name <you@example.com>
2022-06-05 21:21:47 -0700

    second

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    rev = ""
-    for index in range(0, len(my_string)):
-        if my_string[index] != 'c' and my_string[index] != 'C':
-            rev = rev + my_string[index]
-    return 
+    length = len(my_string)
+
+    j = 0
+
+    new_string = my_string[:]
+
+    for i in range(length):
+        if (my_string[i] == 'c' or my_string[i] == 'C'):
+            new_string = new_string[:(i - j)] + my_string[(i + 1):]
+            j += 1
+
+    return (new_string)
